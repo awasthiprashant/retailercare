53641b21b579ceb8dcf75643be9289654810fdf4
Prashant Kr. Awasthi <prashant.kr.awasthi@gmail.com>
2013-01-04 11:31:21 +0530

    dtghfxhf dh fg

--- insurance_master.php
+++ insurance_master.php
@@ -1,5 +1,6 @@
 <?
-//comment 04/01/2013
+//comment 04/01/2013 
+//new comment 04/01/2013 
 if(!isset($_POST['info']) ||!isset($_POST['agentcode']))
 	exit("Error-Required Data Missing");
 require_once "connection.php";
